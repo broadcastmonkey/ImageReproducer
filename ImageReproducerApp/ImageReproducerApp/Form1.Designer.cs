a5ef75c6823050069cf5f10245f36121c73e71cd
Paweł Burgchardt <pawel.burgchardt@blackburst.pl>
2020-03-31 13:51:07 +0200

    init commit

---
+++ ImageReproducerApp/ImageReproducerApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ImageReproducerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbMutationType = new System.Windows.Forms.RadioButton();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(360, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 299);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(3, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 169);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbMutationType);
+            this.groupBox2.Location = new System.Drawing.Point(3, 187);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 100);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mutation Type";
+            // 
+            // rbMutationType
+            // 
+            this.rbMutationType.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rbMutationType.Location = new System.Drawing.Point(6, 19);
+            this.rbMutationType.Name = "rbMutationType";
+            this.rbMutationType.Size = new System.Drawing.Size(110, 38);
+            this.rbMutationType.TabIndex = 0;
+            this.rbMutationType.TabStop = true;
+            this.rbMutationType.Text = "radioButton1";
+            this.rbMutationType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.rbMutationType.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 582);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbMutationType;
+    }
+}
+
